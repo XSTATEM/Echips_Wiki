575c8f8460c9af5ae13ce06ada32b5717e12633e
Vlada <uzakovv41@gmail.com>
2026-02-24 09:06:30 +0300

    Отключила проверку битых ссылок и добавила редактор

--- y/.vitepress/theme/index.ts
+++ y/.vitepress/theme/index.ts
@@ -1,12 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
-import Layout from './Layout.vue' // <--- Добавили импорт
 import StepGuide from '../../components/StepGuide.vue'
 import NewOrder from '../../components/NewOrder.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: Layout, // <--- Указали использовать наш замок
   enhanceApp(ctx: any) {
     ctx.app.component('StepGuide', StepGuide)
     ctx.app.component('NewOrder', NewOrder)
